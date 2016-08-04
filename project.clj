6adb5fdd5bd39eb461f6fb600266b751cdb27caa
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-08-04 15:01:13 -0700

    Adds javaserver and speclj as dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [matts-clojure-ttt "0.1.0-SNAPSHOT"]]
+                 [matts-clojure-ttt "0.1.0-SNAPSHOT"]
+                 [com.github.mh120888/javaserver "1.0-SNAPSHOT"]]
+  :plugins [[speclj "3.3.0"]]
+  :test-paths ["spec"]
   :main ^:skip-aot web-ttt.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles { :dev {:dependencies [[speclj "3.3.0"]]}
+              :uberjar {:aot :all}})
