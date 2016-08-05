0e335f0a1d1a5c194089db722233fd8bb2e07414
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-08-05 11:58:32 -0700

    GET /new-game only returns 200 if all required params are present, returns 422 otherwise

--- spec/web_ttt/core_spec.clj
+++ spec/web_ttt/core_spec.clj
@@ -8,25 +8,17 @@
     (def new-response (.getNewResponse message-factory)))
 
   (context "GET /"
-    (it "returns a 200"
+    (before-all
       (.setRequestLine request "GET / HTTP/1.1")
+      (def app-response (.getResponse basic-app request new-response)))
 
-      (def app-response (.getResponse basic-app request new-response))
-
+    (it "returns a 200"
       (should-contain "200" (.getFormattedResponse app-response)))
 
     (it "returns a content-type of text/html"
-      (.setRequestLine request "GET / HTTP/1.1")
-
-      (def app-response (.getResponse basic-app request new-response))
-
-      (should-contain "Content-Type: text/html" (.getFormattedResponse app-response)))
+     (should-contain "Content-Type: text/html" (.getFormattedResponse app-response)))
 
     (it "returns the contents of the index.html"
-      (.setRequestLine request "GET / HTTP/1.1")
-
-      (def app-response (.getResponse basic-app request new-response))
-
       (should-contain "Do you want to play a game?" (.getFormattedResponse app-response))))
 
   (context "POST /"
@@ -37,13 +29,25 @@
 
       (should-contain "405" (.getFormattedResponse app-response))))
 
-  (context "GET /new-game"
-    (it "returns a 200"
-      (.setRequestLine request "GET /new-game HTTP/1.1")
+  (context "GET /new-game (w/params)"
+    (before-all
+      (.setRequestLine request "GET /new-game?marker=x&gofirst=y&size=3 HTTP/1.1")
+      (def app-response (.getResponse basic-app request new-response)))
 
-      (def app-response (.getResponse basic-app request new-response))
+    (it "returns a 200 if params are present"
+      (should-contain "200" (.getFormattedResponse app-response)))
 
-      (should-contain "200" (.getFormattedResponse app-response))))
+    (it "should return the welcome text"
+      (should-contain "Let's play a game of tic tac toe" (.getFormattedResponse app-response))))
+
+  (context "GET /new-game (w/o params)"
+    (it "returns a 422"
+      (def new-request (.getNewRequest message-factory))
+
+      (.setRequestLine new-request "GET /new-game HTTP/1.1")
+      (def app-response (.getResponse basic-app new-request new-response))
+
+      (should-contain "422" (.getFormattedResponse app-response))))
 
   (context "GET /(any-other-path)"
     (it "returns a 404"
--- src/web_ttt/core.clj
+++ src/web_ttt/core.clj
@@ -1,31 +1,43 @@
 (ns web-ttt.core
   (:gen-class)
   (:require [matts-clojure-ttt.game :as game]
-            [clojure.core.match :as match])
+    [matts-clojure-ttt.console-ui :as ui]
+    [clojure.core.match :as match])
   (:import [server.MyServer]
-           [app.Application]
-           [httpmessage.HTTPResponse]
-           [cobspecapp.CobSpecApp]
-           [basichttpmessage.BasicHTTPMessageFactory]))
+   [app.Application]
+   [httpmessage.HTTPResponse]
+   [cobspecapp.CobSpecApp]
+   [basichttpmessage.BasicHTTPMessageFactory]))
 
 (def message-factory (basichttpmessage.BasicHTTPMessageFactory.))
+
+(deftype WebIO []
+  ui/IOProtocol
+  (io-print-line [type message]
+    (.getBytes message))
+  (io-read [type]
+    (read-line)))
 
 (def basic-app (reify app.Application
   (getResponse [this request response]
     (let [response (.getNewResponse message-factory)
-          method (.getMethod request)
-          path (.getPath request)]
+      method (.getMethod request)
+      path (.getPath request)
+      params (into {} (.getAllParams request))]
       (.setHTTPVersion response "HTTP/1.1")
-      (match/match [method path]
-        ["GET" "/"] (do
-                      (.setStatus response 200)
-                      (.addHeader response "Content-Type" "text/html; charset=utf-8")
-                      (.setBody response (.getBytes (slurp "resources/index.html"))))
-        [_ "/"] (.setStatus response 405)
-        ["GET" "/new-game"] (do
-                      (.setStatus response 200)
-                      (.addHeader response "Content-Type" "text/html; charset=utf-8"))
-        [_ _] (.setStatus response 404))
+      (println params)
+      (match/match [method path params]
+        ["GET" "/" _] (do
+          (.setStatus response 200)
+          (.addHeader response "Content-Type" "text/html; charset=utf-8")
+          (.setBody response (.getBytes (slurp "resources/index.html"))))
+        [_ "/" _] (.setStatus response 405)
+        ["GET" "/new-game" {"size" _ "marker" _ "gofirst" _}] (do
+          (.setStatus response 200)
+          (.addHeader response "Content-Type" "text/html; charset=utf-8")
+          (.setBody response (ui/io-print-line (WebIO.) "Let's play a game of tic tac toe")))
+        ["GET", "/new-game" _] (.setStatus response 422)
+        [_ _ _] (.setStatus response 404))
       response))))
 
 (defn -main
