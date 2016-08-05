d90352cf003f01d98ab13461103ec697e18fd640
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-08-05 12:55:15 -0700

    Creates app namespace and moves the internals of implementation of Application interface from core to app

---
+++ src/web_ttt/app.clj
@@ -0,0 +1,38 @@
+(ns web-ttt.app
+  (:require [matts-clojure-ttt.game :as game]
+    [matts-clojure-ttt.console-ui :as ui]
+    [clojure.core.match :as match])
+  (:import [server.MyServer]
+   [app.Application]
+   [httpmessage.HTTPResponse]
+   [cobspecapp.CobSpecApp]
+   [basichttpmessage.BasicHTTPMessageFactory]))
+
+(def message-factory (basichttpmessage.BasicHTTPMessageFactory.))
+
+(deftype WebIO []
+  ui/IOProtocol
+  (io-print-line [type message]
+    (.getBytes message))
+  (io-read [type]
+    (read-line)))
+
+(defn getResponse [request response]
+  (let [response (.getNewResponse message-factory)
+    method (.getMethod request)
+    path (.getPath request)
+    params (into {} (.getAllParams request))]
+    (.setHTTPVersion response "HTTP/1.1")
+    (match/match [method path params]
+      ["GET" "/" _] (do
+        (.setStatus response 200)
+        (.addHeader response "Content-Type" "text/html; charset=utf-8")
+        (.setBody response (.getBytes (slurp "resources/index.html"))))
+      [_ "/" _] (.setStatus response 405)
+      ["GET" "/new-game" {"size" _ "marker" _ "gofirst" _}] (do
+        (.setStatus response 200)
+        (.addHeader response "Content-Type" "text/html; charset=utf-8")
+        (.setBody response (ui/io-print-line (WebIO.) "Let's play a game of tic tac toe")))
+      ["GET", "/new-game" _] (.setStatus response 422)
+      [_ _ _] (.setStatus response 404))
+    response))
--- src/web_ttt/core.clj
+++ src/web_ttt/core.clj
@@ -1,6 +1,7 @@
 (ns web-ttt.core
   (:gen-class)
-  (:require [matts-clojure-ttt.game :as game]
+  (:require [web-ttt.app :as app]
+    [matts-clojure-ttt.game :as game]
     [matts-clojure-ttt.console-ui :as ui]
     [clojure.core.match :as match])
   (:import [server.MyServer]
@@ -11,34 +12,9 @@
 
 (def message-factory (basichttpmessage.BasicHTTPMessageFactory.))
 
-(deftype WebIO []
-  ui/IOProtocol
-  (io-print-line [type message]
-    (.getBytes message))
-  (io-read [type]
-    (read-line)))
-
 (def basic-app (reify app.Application
   (getResponse [this request response]
-    (let [response (.getNewResponse message-factory)
-      method (.getMethod request)
-      path (.getPath request)
-      params (into {} (.getAllParams request))]
-      (.setHTTPVersion response "HTTP/1.1")
-      (println params)
-      (match/match [method path params]
-        ["GET" "/" _] (do
-          (.setStatus response 200)
-          (.addHeader response "Content-Type" "text/html; charset=utf-8")
-          (.setBody response (.getBytes (slurp "resources/index.html"))))
-        [_ "/" _] (.setStatus response 405)
-        ["GET" "/new-game" {"size" _ "marker" _ "gofirst" _}] (do
-          (.setStatus response 200)
-          (.addHeader response "Content-Type" "text/html; charset=utf-8")
-          (.setBody response (ui/io-print-line (WebIO.) "Let's play a game of tic tac toe")))
-        ["GET", "/new-game" _] (.setStatus response 422)
-        [_ _ _] (.setStatus response 404))
-      response))))
+    (app/getResponse request response))))
 
 (defn -main
   [& args]
