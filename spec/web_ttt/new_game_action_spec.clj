993019edb940afc05c3e9cca761b3173c12ae65a
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-08-08 15:09:20 -0700

    User can now select options (order of players, marker, board size) and start a game - parameters sent with the new game request are updated appropriately based on the user's choices.

--- spec/web_ttt/new_game_action_spec.clj
+++ spec/web_ttt/new_game_action_spec.clj
@@ -16,4 +16,9 @@
   (it "returns a response with a status of 422 w/incorrect parameters"
     (.setRequestLine request "GET /new-game?marker=incorrect&gofirst=incorrect&size=randomwords HTTP/1.1")
     (get-response request response)
+    (should-contain "422" (.getFormattedResponse response)))
+
+  (it "returns a response with a status of 422 w/missing parameters"
+    (.setRequestLine request "GET /new-game HTTP/1.1")
+    (get-response request response)
     (should-contain "422" (.getFormattedResponse response))))
