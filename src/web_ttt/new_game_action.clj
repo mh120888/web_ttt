098f0d08b2e2f54bd86985b9ec1fbca24794fd46
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-08-09 14:40:51 -0700

    Refactors #get-response to remove duplication - no functional changes.

--- src/web_ttt/new_game_action.clj
+++ src/web_ttt/new_game_action.clj
@@ -9,21 +9,13 @@
   (io-read [type]
     (read-line)))
 
-
 (defn get-response [request response]
   (let [params (into {} (.getAllParams request))]
     (.setHTTPVersion response "HTTP/1.1")
     (match/match [params]
-      [{"size" "3", "marker" (:or "x" "o"), "gofirst" (:or "y" "n")}]
-      (do
-        (.setStatus response 200)
-        (.addHeader response "Content-Type" "text/html; charset=utf-8")
-        (.setBody response (ui/io-print-line (WebIO.) (str "Let's play a game of tic tac toe" (slurp "resources/_empty_board_3.html")))))
-      [{"size" "4", "marker" (:or "x" "o"), "gofirst" (:or "y" "n")}]
-      (do
-        (.setStatus response 200)
-        (.addHeader response "Content-Type" "text/html; charset=utf-8")
-        (.setBody response (ui/io-print-line (WebIO.) (str "Let's play a game of tic tac toe" (slurp "resources/_empty_board_4.html")))))
-    [_]
-    (do
-      (.setStatus response 422)))))
+      [{"size" (:or "3" "4"), "marker" (:or "x" "o"), "gofirst" (:or "y" "n")}]
+        (do
+          (.setStatus response 200)
+          (.addHeader response "Content-Type" "text/html; charset=utf-8")
+          (.setBody response (ui/io-print-line (WebIO.) (str "Let's play a game of tic tac toe" (slurp (str "resources/_empty_board_" (get params "size") ".html"))))))
+      [_] (.setStatus response 422))))
